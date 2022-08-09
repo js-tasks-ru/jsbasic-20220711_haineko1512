afcaf9ac42c1f52a4b6ed322e7c4e90db3a02c39
Anna Fadeeva <haineko1512@mail.ru>
2022-08-09 21:03:03 +0300

    task 6.1: add render userTable class

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,53 @@
  *
  */
 export default class UserTable {
+  // rows = [];
+  table;
+  headerHeadlines = {name : "Имя", age : "Возвраст", salary : "Вес", city : "Город"};
+
   constructor(rows) {
+    this.rows = rows;
+    this.table = document.createElement('table');
+    this.elem = this.elem();
+  }
+
+  elem() {
+    this.#renderHead();
+    this.#renderBody();
+    return this.table;
+  }
+
+  get headTitle() {
+    return Object.values(this.headerHeadlines);
+  }
+
+  #renderHead() {
+    let rowHead = this.table.createTHead().insertRow(0);
+    this.headTitle.forEach((item, index) => rowHead.insertCell().innerHTML = this.headTitle[index]);
+    rowHead.insertCell(rowHead.length).innerHTML = '';
+    return rowHead;
+  }
+
+  #renderBody() {
+    let body = this.table.createTBody();
+    for ( let row of this.rows ) {
+      let newRow = body.insertRow();
+      Object.values(row).forEach((item) => newRow.insertCell().innerHTML = item);
+      newRow.insertCell(newRow.lenght).appendChild(this.#btn());
+    }
+    let btns = body.querySelectorAll('.btn');
+    btns.forEach((btn) => btn.addEventListener('click', this.#deleteRow));
+  }
+
+  #deleteRow(event) {
+    event.currentTarget.parentElement.parentElement.remove();
+  }
+
+  #btn() {
+    let btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.textContent = 'X';
+    return btn;
   }
 }
+
